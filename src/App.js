5dcd5abd77294c34d19ed0ddac7bc5d6effd0faa
p121s <p121s@mail.ru>
2021-10-27 16:58:35 +0300

    Fix code App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ModalSwitch from './components/Routing/ModalSwich/ModalSwich';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import './App.scss';
-
-const notifySetting = {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-};
-
-export const notifySuccess = (message) => toast.success(`${message}`, notifySetting);
-export const notifyError = (error) => toast.error(`${error}`, notifySetting);
 
 function App() {
 
