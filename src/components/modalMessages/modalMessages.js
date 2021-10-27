a501841858f935c8b813da69ff34f4cd866a17e5
p121s <p121s@mail.ru>
2021-10-27 16:51:48 +0300

    Create modul Modal messages

---
+++ src/components/modalMessages/modalMessages.js
@@ -0,0 +1,14 @@
+import { toast } from 'react-toastify';
+
+const notifySetting = {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+};
+
+export const notifySuccess = (message) => toast.success(`${message}`, notifySetting);
+export const notifyError = (error) => toast.error(`${error}`, notifySetting);
